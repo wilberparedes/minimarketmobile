4b9859d9fc657179d9c7d324cab3258f85bbb320
Wilber Paredes <wilberparedes@gmail.com>
2026-08-01 12:39:04 -0500

    feat(ui): add errorview component

---
+++ src/components/ErrorView/ErrorView.styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 24,
+  },
+  message: {
+    fontSize: 16,
+    textAlign: "center",
+    marginBottom: 16,
+  },
+})
